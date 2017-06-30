1268bef9ef6709b7abfa902054d985c1477a0169
Crishtian Ramírez <cristhianramirezgd@gmail.com>
2017-06-30 11:33:10 -0500

    {html} Crea html para header y footer de checkout

---
+++ chekout-custom.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<title>Header chekout</title>
+	<link rel="stylesheet" type="text/css" href="https://io.vtex.com.br/front-libs/bootstrap/2.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://io.vtex.com.br/front-libs/bootstrap/2.3.2/css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="speed/src/arquivos/checkout-custom.css">
+</head>
+<body>
+	<nav class="row position-fixed-top shadow">
+		<div class="top-bar">
+			
+		</div>
+		<div class="container">
+			<div class="navbar">
+			 <ul class="nav">
+			   <li class="logo">
+			     <a class="brand" href="/"><img src="/arquivos/monkey-market-juguetes-logo-scroll-down.png"/></a>
+			   </li>
+			 </ul>
+			</div>
+		</div>
+		<div class="container">
+			<h2 class="title-font text-center">Divierteté comprando</h2>
+		</div>
+		<div class="row red-bg h-20"></div>
+	</nav>
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<footer class="footer">
+		<div class="row red-bg h-20 icon-monkey relative"></div>
+		<br />
+		<div class="container">
+			<section class="row text-center large-margin-top large-margin-bottom">
+				<div class="span4">
+					<img src="/arquivos/monkey-market-tienda-icono-04.png" alt="Envíos gratis" class="rounded" />
+					<p><small><span>Envíos gratis*</span></small></p>
+				</div>
+				<div class="span4">
+					<img src="/arquivos/monkey-market-tienda-icono-03.png" alt="Pago contraentrega" class="rounded" />
+					<p><small><span>Pago contraentrega*</span></small></p>
+				</div>
+				<div class="span4">
+					<img src="/arquivos/monkey-market-tienda-icono-11.png" alt="Devoluciones Gratis" class="rounded" />
+					<p><small><span>Devoluciones Gratis*</span></small></p>
+				</div>
+			</section>
+		</div>
+				<section class="container-fluid bg-grey">
+					<div class="container-fluid">
+						<!-- Main container -->
+						<div class="level">
+						  <!-- Left side -->
+						  <div class="level-left">
+						    <div class="level-item img-middle large-margin-bottom large-margin-top">
+						    	<p>
+						    		<small>Implementación <b>Vtex</b> por <a href="https://designplus.co/"><b>DesignPlus</b> <img src="http://cdn.designplus.co/_img/white-made-by-designplus.png" width="16" height="15" alt="DesignPlus"> © 2017</a></small>
+						    </p>
+						    </div>
+						  </div>
+						  <!-- Right side -->
+						  <div class="level-right large-margin-bottom vtex-seal">
+						    <figure>
+						    		<img class="small-image-center img-responsive pull-right small-float-none" src="/arquivos/logo-footer-vtex-monkey-market-tienda-gray-lighten.png" alt="monkey market.com shop" />
+						    </figure>
+						  </div>
+						</div>
+					</div>
+				</section>
+				<hr />
+		  		<div class="text-center">Todos los derechos reservados MonkeyMarket.com</div>
+		    <hr />
+	</footer>
+</body>
+</html>
+
+
