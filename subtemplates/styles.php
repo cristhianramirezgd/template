25cb971d22da7f446ec08d7da75daa0e04c74a47
Cristhian Ramírez <cramirez@tbwacolombia.com>
2018-04-26 11:48:56 -0500

    Estilo: Añade css para la libreria de fancybox

--- subtemplates/styles.php
+++ subtemplates/styles.php
@@ -1,3 +1,4 @@
 <!-- MAIN CSS -->
+	<link rel="stylesheet" type="text/css" href="https://jumbocolombiafood.vtexcommercestable.com.br/arquivos/jquery.fancybox.min.css" />
     <link rel="stylesheet" type="text/css" href="/speed/build/arquivos/app-mundo-saludable.min.css" />
 </head>
