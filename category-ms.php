fe4e81025a1b643db3fe0ddb9f8fef2ac7712cd8
Cristhian Ramírez <cramirez@tbwacolombia.com>
2018-05-03 11:53:04 -0500

    Estilo: crea contenedores para vista de catagoria

---
+++ category-ms.php
@@ -0,0 +1,170 @@
+    <?php require_once("subtemplates/head.php"); ?>
+    <?php require_once("subtemplates/styles.php"); ?>
+    <?php require_once("subtemplates/header.php"); ?>
+    <h2 class="title-section-ms"><span><i class="icon icon-blog"></i>Blog</span></h2>
+    <main class="mundo-saludable-v2 catagory-ms">
+      <aside>
+        <label>check</label>
+        <input type="checkbox" />
+        <label>check</label>
+        <input type="checkbox" />
+        <label>check</label>
+        <input type="checkbox" />
+      </aside>
+      <section class="wrapper container-main container-category-ms">
+        <div class="container-card-article">
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <?php require_once("subtemplates/footer.php"); ?>
+    <?php require_once("subtemplates/scripts.php"); ?>
+    <!-- librearies -->
+    <script type="text/javascript" src="https://jumbocolombiafood.vtexcommercestable.com.br/arquivos/jquery.fancybox.min.js" charset="utf8"></script>
+    <script type="text/javascript" src="https://jumbocolombiafood.vteximg.com.br/arquivos/slick.min.js" charset="utf8"></script>
+    <script type="text/javascript" src="/speed/build/arquivos/app-mundosaludable.js" charset="utf8"></script>  
+  </body>
+</html>
