446a24c5bbe588b11c06ae919e9e73b21e399bbc
Cristhian Ramírez <cramirez@tbwacolombia.com>
2018-05-04 12:27:37 -0500

    Estilo: crea componente de articulo

---
+++ article.php
@@ -0,0 +1,202 @@
+    <?php require_once("subtemplates/head.php"); ?>
+    <?php require_once("subtemplates/styles.php"); ?>
+    <?php require_once("subtemplates/header.php"); ?>
+    <h1 class="title-section-ms section-margin"><span>Título de Artículo</span></h1>
+    <main class="mundo-saludable-v2 article-ms">
+      <section class="section-article">
+      	<div class="article-banner section-margin">
+      		<img src="https://fakeimg.pl/1366x500/" data-mobile="https://fakeimg.pl/400x400/" />
+      		<div class="banner-article-info">
+      			<div class="tags-article-info">
+      				<span class="slug-article bg-gradient-1">Yo Puedo</span>
+      				<span class="slug-article bg-gradient-2">Yo Decidí</span>
+      				<span class="slug-article bg-gradient-3">Yo Quiero</span>
+      			</div>
+      			<div class="media-article-info">
+      				<div class="views-article">
+      					<span><i class="icon-heart"></i><small class="text-dark">1.987</small></span>
+      				</div>
+      				<div class="social-media">
+      					<a href=""><i class="icon-facebook"></i></a>
+      					<a href=""><i class="icon-instagram"></i></a>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<article class="container-main article-content">
+      		<h4>Subtítulo</h4>
+      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque officia neque non magni ab a debitis sunt fuga optio obcaecati! Sint soluta, eius? Consectetur animi nostrum aliquid error omnis. Tenetur voluptate in eos perferendis quae eum veritatis commodi, consequuntur dolore molestias, accusamus ullam iure, exercitationem consectetur maiores repellendus quaerat. At fuga labore nobis accusamus ad nulla repellat similique, quo consectetur esse iure doloremque odit culpa quasi enim laboriosam, aut magni pariatur voluptas. Odio dolorem illo consectetur quisquam quaerat vitae est numquam! Vitae possimus eaque iure nam. Laboriosam et tempora nobis eum accusantium! Impedit natus mollitia deserunt tenetur doloribus iusto sed, rem repellat molestiae consequatur nemo, debitis delectus perspiciatis eaque? Maiores quasi, culpa laudantium distinctio temporibus incidunt harum porro doloremque vero laborum, repellendus. Animi aut temporibus, <a href="">illum consequatur aspernatur</a> sapiente culpa omnis iusto tempora explicabo non, voluptates quibusdam neque magni recusandae hic! Quidem sunt totam deleniti ipsum nobis. Illum quisquam perferendis pariatur nobis dicta mollitia quidem dignissimos quasi.</p>
+      		<img src="https://fakeimg.pl/400x400/">
+      		<img src="https://fakeimg.pl/400x400/">
+      		<p>sapiente atque ipsa labore rerum debitis repudiandae voluptates nam consectetur, corrupti earum, nemo commodi, necessitatibus. Eveniet repellendus aspernatur ex aliquid, nemo, tempora optio, sint quos impedit asperiores magni architecto perspiciatis? Repellat officiis, alias quam perferendis veritatis porro optio culpa numquam <a href="">illum consequatur aspernatur</a>, nemo iusto, nam consequuntur. Atque dignissimos nulla, aperiam placeat, temporibus amet illo cum voluptatibus ex. Iste laboriosam illo corporis sit possimus laudantium expedita non tenetur facere nobis itaque, fugiat nulla quia voluptates quasi ab temporibus nemo, quaerat obcaecati quae at alias earum odio! Assumenda impedit quo nihil deleniti debitis ex eius tempore, a iste illum laboriosam numquam fugiat, voluptate <a href="">illum consequatur aspernatur</a> autem dolorum. Ratione ipsam, commodi corporis. Cumque voluptas blanditiis voluptate aperiam eos dolorum totam! Officiis magni modi provident voluptatum, minus delectus esse deleniti, iure doloribus! Soluta maxime autem nemo. Natus, alias exercitationem culpa, suscipit officia recusandae beatae.</p>
+      	</article>
+      </section>
+      <section class="wrapper container-main">
+        <h2 class="title-section-ms"><span><i class="icon icon-blog"></i>Artículos Relacionados</span></h2>
+        <div class="container-main container-card-article">
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+          <div class="card-ms card-article-ms">
+            <div class="card-img-ms">
+              <span class="card-label-filter-ms">VEGANO, DIABÉTICO</span>
+              <img src="https://fakeimg.pl/300x300/" alt=""/>
+            </div>
+            <div class="card-text-ms">
+              <a href="">
+                <h6 class="text-bold">Excepteur sint occaecat cupidatat non proident, sunt in culpa.</h6>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+
+              </a>
+            <div class="card-interaction-ms">
+              <span><i class="icon-heart"></i>
+                <small class="text-dark">1.987</small></span>
+                <a class="text-jumbogreen" href=""><span>Ver<i class="icon-plus"></i></span></a>
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </main>
+    <?php require_once("subtemplates/footer.php"); ?>
+    <?php require_once("subtemplates/scripts.php"); ?>
+    <!-- librearies -->
+    <script type="text/javascript" src="https://jumbocolombiafood.vtexcommercestable.com.br/arquivos/jquery.fancybox.min.js" charset="utf8"></script>
+    <script type="text/javascript" src="https://jumbocolombiafood.vteximg.com.br/arquivos/slick.min.js" charset="utf8"></script>
+    <script type="text/javascript" src="/speed/build/arquivos/app-mundosaludable.js" charset="utf8"></script>
+    <script type="text/javascript">
+    	 // slick slider
+      $('.container-card-article').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        adaptiveHeight: true,
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 640,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true
+              }
+            }
+          ]
+      });
+    </script>
+  </body>
+</html>
