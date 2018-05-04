62bc069e111f5bc8cd3913763265bf46b9d6092b
Cristhian Ramírez <cramirez@tbwacolombia.com>
2018-05-04 11:49:21 -0500

    Estilo: Cambia script para interchange global

--- speed/src/arquivos/app-mundosaludable.js
+++ speed/src/arquivos/app-mundosaludable.js
@@ -1,13 +1,5 @@
 $(document).ready(function(a) {
-        /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a(".container-banner-middle img").each(function() {
-            var b = a(this).attr("data-mobile");
-            a(this).attr("src", b)
-        });
-        /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a(".bg-banner img.bg-img").each(function() {
-            var b = a(this).attr("data-mobile");
-            a(this).attr("src", b)
-        });
-        /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a(".container-banner-bottom img").each(function() {
+        /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a("img").each(function() {
             var b = a(this).attr("data-mobile");
             a(this).attr("src", b)
         });
