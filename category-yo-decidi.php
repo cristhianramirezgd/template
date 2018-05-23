00d13063e953655ff6238ed6db5ba5b414b731d6
Cristhian Ramírez <cramirez@tbwacolombia.com>
2018-05-23 14:56:44 -0500

    Estilo: Crea filtros para articulos de blog

--- category-yo-decidi.php
+++ category-yo-decidi.php
@@ -2,7 +2,7 @@
     <?php require_once("subtemplates/styles.php"); ?>
     <?php require_once("subtemplates/header.php"); ?>
     <section id="titleCategorie"></section>
-    <main class="mundo-saludable-v2">
+    <main class="mundo-saludable-v2 category-ms">
       <section class="catagory-ms">
         <aside class="wrapper-filter">
         <div class="sticky-filter-ms">
@@ -10,7 +10,8 @@
         </div>
       </aside>
       <div class="container-category-ms" id="container-pagination">
-        <ul class="container-card-article" id="cardsMs"></ul>
+        <small id="output"></small>
+        <ul id="cardsMs"></ul>
       </div>
       </section>
     </main>
@@ -19,6 +20,7 @@
     <!-- librearies -->
     <script type="text/javascript" src="https://jumbocolombiafood.vtexcommercestable.com.br/arquivos/jquery.fancybox.min.js" charset="utf8"></script>
     <script type="text/javascript" src="https://jumbocolombiafood.vteximg.com.br/arquivos/slick.min.js" charset="utf8"></script>
+     <script type="text/javascript" src="https://jumbocolombiafood.vteximg.com.br/arquivos/isotope-docs.min.js" charset="utf8"></script>
     <script type="text/javascript" src="/speed/build/arquivos/app-mundosaludable.js" charset="utf8"></script>
   </body>
 </html>
--- speed/src/arquivos/app-mundosaludable.js
+++ speed/src/arquivos/app-mundosaludable.js
@@ -36,120 +36,12 @@ $(document).ready(function(a) {
             var slug;
             var nameSlugTagFilter;
             var slugSlugTagFilter;
-            var slugTag;
-            var nameTag;
             var categoriesTag;
             var resume;
             var $items;
 
-    function filter(url){
-        $('#filterTitleCategorie').append('<h6 class="filter-title-ms">Filtrar Por</h6>');
-        $( ".filter-title-ms" ).on( "click", function(e) {
-                e.preventDefault();
-                var $this = $(this);
-                $this.parents('.container-filter-ms').toggleClass( "active-ms");
-                });
-        var ct =''
-        for (var i = 0; i < 1; i++) {
-            if (url[i].e_categories.length == 2) {
-                ct += url[i].e_categories[1].name
-            
-            }
-            else if(url[i].e_categories.length == 1) {
-                 ct += url[i].e_categories[0].name
-                }
-            else{
-                 ct += 'Sin Categoria'
-                }
-        }
-        $('#titleCategorie').append('<h2 class="title-section-ms">' 
-             + '<span>' 
-             + '<i class="icon icon-blog">' 
-             + '</i>' 
-             + ct
-             + '</span>' 
-             + '</h2>');
-        createItems(url);
-        function createItems(slugTag,nameTag) {
-        for (  var i=0; i < url.length; i++ ) {
-             for ( var j=0; j < url[i].e_tags.length; j++ ) {
-                var slugTag = url[i].e_tags[j].slug;
-                var nameTag = url[i].e_tags[j].name;
-                $('#filterTitleCategorie').append(
-                    '<div class="filters-ms">'
-                    +'<label>'
-                    + nameTag
-                    +'</label>'
-                    +'<input type="checkbox" class="filtervalue" value="' + slugTag +'"/>'
-                    +'</div>'
-                );
-             }
-        }
-        // init Isotope
-        var $container = $('#cardsMs').isotope({
-          itemSelector: '.card-article-ms'
-        });
-
-        var $output = $('#output');
-
-        // filter with selects and checkboxes
-        var $checkboxes = $('.filtervalue');
-
-        $checkboxes.change( function() {
-          // map input values to an array
-          var inclusives = [];
-          // inclusive filters from checkboxes
-          $checkboxes.each( function( i, elem ) {
-            // if checkbox, use value if checked
-            if ( elem.checked ) {
-              inclusives.push( elem.value );
-            }
-          });
-
-          // combine inclusive filters
-          var filterValue = inclusives.length ? inclusives.join(', ') : '*';
-
-          $output.text( filterValue );
-          $container.isotope({ filter: filterValue })
-        });
-        
-            var $items;
-            // create one item for each
-            
-            var $item = cards(url);
-            // var $item = cardItem();
-            
-            // function cardItem(url){
-            //  $('#cardsMs').append('<li>'
-            //       + '<div class="'+ slugTag +'  card-ms card-article-ms ">'
-            //       + '<div class="card-img-ms">'
-            //       + '<span class="card-label-filter-ms">'+ nameTag +'</span>'
-            //       + '<img src="' + imgCover + '" />'
-            //       + '</div>'
-            //       + '<div class="card-text-ms">'
-            //       + '<a href="/eliteplayers/noticias/entradas/?/'+ link +'">'
-            //       + '<h6 class="text-bold">' + title + '</h6>'
-            //       + resume
-            //       + '</a>'
-            //       + '<div class="card-interaction-ms">'
-            //       + '<span><i class="icon-heart"></i>'
-            //       + '<small class="text-dark">1.987</small></span>'
-            //       + '<a class="text-jumbogreen" href="'+ link +'"><span>Ver<i class="icon-plus"></i></span></a>'
-            //       + '</div>'
-            //       + '</div>'
-            //       + '</div>'
-            //       + '</li>'
-            //       );
-            // }
-
-            $items = $items ? $items.add( $item ) : $item;
-        }
-    }
-
         function cards(url){
-        
         $.each(url, function(index, post) {
-
             var i = 0;
             if (jQuery.inArray( i, post.categories ) != 1) {
                 i++
@@ -180,8 +72,6 @@ $(document).ready(function(a) {
                 // console.log(slugTag)
                 // console.log(nameTag)
                 console.log(categoriesTag)
-                // function isotopeFilter(url){
-
                 switch (i) {
                     case 1:
                         $('#cardsMs').append('<li>'
@@ -294,17 +184,248 @@ $(document).ready(function(a) {
                 ]
             });
     };
-            $.get(urlPosts, function (homepost) {
-                // if (window.location.pathname.indexOf("wordpress") != -1){
-                    cards(homepost);
 
-                // };
+
+    //FIltros Isotope para Categorias
+    function filterCards(url){
+        function cardItem(){
+             $.each(url, function(index, post) {
+            var slugTag;
+            var nameTag;
+                var i = 0;
+                if (jQuery.inArray( i, post.categories ) != 1) {
+                    i++
+                    console.log(i)
+                    console.log(this.title['rendered']);
+                    var imgCover = this.better_featured_image.media_details.sizes.thumbnail['source_url'];
+                    var link = this.link.replace("https://catalogostiendasjumbo.co/tiendasjumboblog/","");
+                    var title = this.title['rendered'];
+                    var slug = this.slug;
+                    var nameSlugTagFilter = this.e_tags[0].name;
+                    var slugSlugTagFilter = this.e_tags[0].slug;
+                    var slugTag = '';
+                        $.each(this.e_tags, function(sTags, item) {
+                           slugTag += ' ';
+                           slugTag += item.slug;
+                        });
+                    var nameTag = '';
+                        $.each(this.e_tags, function(nTags, item) {
+                           nameTag += item.name;
+                           nameTag += ', ';
+                        });
+                    var categoriesTag = '';
+                        $.each(this.e_categories, function(nCategories, item) {
+                           categoriesTag += item.name;
+                           categoriesTag += ', ';
+                        });
+                    var resume = this.excerpt['rendered'];
+                    // console.log(slugTag)
+                    // console.log(nameTag)
+                    console.log(categoriesTag)
+                    switch (i) {
+                        case 1:
+                            $('#cardsMs').append('<li class="card-ms card-article-ms'+ slugTag +'">'
+                                 + '<div>'
+                                 + '<div class="card-img-ms">'
+                                 + '<span class="card-label-filter-ms">'+ nameTag +'</span>'
+                                 + '<img src="' + imgCover + '" />'
+                                 + '</div>'
+                                 + '<div class="card-text-ms">'
+                                 + '<a href="/eliteplayers/noticias/entradas/?/'+ link +'">'
+                                 + '<h6 class="text-bold">' + title + '</h6>'
+                                 + resume
+                                 + '</a>'
+                                 + '<div class="card-interaction-ms">'
+                                 + '<span><i class="icon-heart"></i>'
+                                 + '<small class="text-dark">1.987</small></span>'
+                                 + '<a class="text-jumbogreen" href="'+ link +'"><span>Ver<i class="icon-plus"></i></span></a>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</li>'
+                                 );
+                            break;
+                        case 2:
+                            $('#cardsMs').append('<li>'
+                                 + '<div class="card-ms card-article-ms">'
+                                 + '<div class="card-img-ms">'
+                                 + '<span class="card-label-filter-ms"></span>'
+                                 + '<img src="' + imgCover + '" />'
+                                 + '</div>'
+                                 + '<div class="card-text-ms">'
+                                 + '<a href="/eliteplayers/noticias/entradas/?/'+ link +'">'
+                                 + '<h6 class="text-bold">' + title + '</h6>'
+                                 + resume
+                                 + '</a>'
+                                 + '<div class="card-interaction-ms">'
+                                 + '<span><i class="icon-heart"></i>'
+                                 + '<small class="text-dark">1.987</small></span>'
+                                 + '<a class="text-jumbogreen" href="'+ link +'"><span>Ver<i class="icon-plus"></i></span></a>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</li>'
+                                 );
+                            break;
+                        case 3:
+                            $('#cardsMs').append('<li>'
+                                 + '<div class="'+ slugTag +'  card-ms card-article-ms ">'
+                                 + '<div class="card-img-ms">'
+                                 + '<span class="card-label-filter-ms">'+ nameTag +'</span>'
+                                 + '404 Image not found"'
+                                 + '</div>'
+                                 + '<div class="card-text-ms">'
+                                 + '<a href="/eliteplayers/noticias/entradas/?/'+ link +'">'
+                                 + '<h6 class="text-bold">' + title + '</h6>'
+                                 + resume
+                                 + '</a>'
+                                 + '<div class="card-interaction-ms">'
+                                 + '<span><i class="icon-heart"></i>'
+                                 + '<small class="text-dark">1.987</small></span>'
+                                 + '<a class="text-jumbogreen" href="'+ link +'"><span>Ver<i class="icon-plus"></i></span></a>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</li>'
+                                 );
+                            break;
+                        case 4:
+                            $('#cardsMs').append('<li>'
+                                 + '<div class="card-ms card-article-ms ">'
+                                 + '<div class="card-img-ms">'
+                                 + '<span class="card-label-filter-ms"></span>'
+                                 + '<img src="" alt="404 Image not found"/>'
+                                 + '</div>'
+                                 + '<div class="card-text-ms">'
+                                 + '<a href="/eliteplayers/noticias/entradas/?/'+ link +'">'
+                                 + '<h6 class="text-bold">' + title + '</h6>'
+                                 + resume
+                                 + '</a>'
+                                 + '<div class="card-interaction-ms">'
+                                 + '<span><i class="icon-heart"></i>'
+                                 + '<small class="text-dark">1.987</small></span>'
+                                 + '<a class="text-jumbogreen" href="'+ link +'"><span>Ver<i class="icon-plus"></i></span></a>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</div>'
+                                 + '</li>'
+                                 );
+                            break;
+                        default:
+                    }
+                }
+            });
+        }
+        $('#filterTitleCategorie').append('<h6 class="filter-title-ms">Filtrar Por</h6>');
+        $( ".filter-title-ms" ).on( "click", function(e) {
+                e.preventDefault();
+                var $this = $(this);
+                $this.parents('.container-filter-ms').toggleClass( "active-ms");
+                });
+        var ct =''
+        for (var i = 0; i < 1; i++) {
+            if (url[i].e_categories.length == 2) {
+                ct += url[i].e_categories[1].name
+            
+            }
+            else if(url[i].e_categories.length == 1) {
+                 ct += url[i].e_categories[0].name
+                }
+            else{
+                 ct += 'Sin Categoria'
+                }
+        }
+        $('#titleCategorie').append('<h2 class="title-section-ms">' 
+             + '<span>' 
+             + '<i class="icon icon-blog">' 
+             + '</i>' 
+             + ct
+             + '</span>' 
+             + '</h2>');
+        createItems(url);
+        function createItems(slugTag,nameTag) {
+            for (  var i=0; i < url.length; i++ ) {
+                 for ( var j=0; j < url[i].e_tags.length; j++ ) {
+                    var slugTag = url[i].e_tags[j].slug;
+                    var nameTag = url[i].e_tags[j].name;
+                    $('#filterTitleCategorie').append(
+                        '<div class="filters-ms">'
+                        +'<label>'
+                        + nameTag
+                        +'</label>'
+                        +'<input type="checkbox" class="filtervalue" value=".' + slugTag +'"/>'
+                        +'</div>'
+                    );
+                 }
+            }
+            
+
+            var $output = $('#output');
+            var $items;
+            var $item = cardItem(url);
+            // create one item for each
+
+           // init Isotope
+           var $container = $('#cardsMs').isotope({
+             itemSelector: '.card-article-ms'
+           });
+            // filter with selects and checkboxes
+            var $checkboxes = $('.filtervalue');
+
+            $checkboxes.change( function() {
+              // map input values to an array
+              var inclusives = [];
+              // inclusive filters from checkboxes
+              $checkboxes.each( function( i, elem ) {
+                // if checkbox, use value if checked
+                if ( elem.checked ) {
+                  inclusives.push( elem.value );
+                }
+              });
+
+              // combine inclusive filters
+              var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+
+              $output.text( filterValue );
+              $container.isotope({ filter: filterValue })
+            });
+            $items = $items ? $items.add( $item ) : $item;
+        }
+    }
+    // callbacks pages
+            // $.get(urlPosts, function (homepost) {
+            //     if (window.location.pathname.indexOf("index.php") != -1){
+            //         cards(homepost);
+            //         // filterCards(homepost);
+
+            //     };
+            // });
+            $.get(urlPosts, function (homepost) {
+                if (window.location.pathname.indexOf("category-ms") != -1){
+                    // cards(homepost);
+                    filterCards(homepost);
+
+                };
             });
             $.get(c3, function (yoDecidi) {
                 if (window.location.pathname.indexOf("yo-decidi") != -1){
                     // cards(yoDecidi);
                     // createItems(yoDecidi);
-                    filter(yoDecidi);
+                    filterCards(yoDecidi);
+                };
+            });
+            $.get(c4, function (yoQuiero) {
+                if (window.location.pathname.indexOf("yo-quie") != -1){
+                    // cards(yoQuiero);
+                    // createItems(yoQuiero);
+                    filterCards(yoQuiero);
+                };
+            });
+            $.get(c2, function (yoPuedo) {
+                if (window.location.pathname.indexOf("yo-puedo") != -1){
+                    // cards(yoPuedo);
+                    // createItems(yoPuedo);
+                    filterCards(yoPuedo);
                 };
             });
     })(jQuery);
